9de914445345e0b2109c56fe779a8ca6a80452df
Jeremy.G <jeremy.giusti.fr@gmail.com>
2017-03-22 11:29:50 +0100

    sprite generator lib done
    not tested
    optimisation may be needed

--- spritesheetgenerator/src/main/java/com/sqli/spritesheetgenerator/SpriteGenerator.java
+++ spritesheetgenerator/src/main/java/com/sqli/spritesheetgenerator/SpriteGenerator.java
@@ -3,6 +3,7 @@ package com.sqli.spritesheetgenerator;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.AsyncTask;
 
 import com.sqli.spritesheetgenerator.model.Sprite;
 import com.sqli.spritesheetgenerator.model.SpriteSheet;
@@ -15,25 +16,72 @@ import java.util.ArrayList;
 
 public class SpriteGenerator {
 
+    /**
+     * generate a single bitmap spritesheet from the spriteSheet model data <br>
+     * TODO optimizations if needed
+     *
+     * @param spritSheetModel model with all data needed to make the bitmap
+     * @return
+     */
     public static Bitmap generateSpriteSheet(SpriteSheet spritSheetModel) {
-        Bitmap result = Bitmap.createBitmap(spritSheetModel.getmSpriteColumnNb(), spritSheetModel.getmSpriteRowNb(), Bitmap.Config.ARGB_8888);
+
+        int spriteHeight = spritSheetModel.getSpriteHeight();
+        int spriteWidth = spritSheetModel.getSpriteWidth();
+
+        Bitmap result = Bitmap.createBitmap(spritSheetModel.getmSpriteColumnNb() * spriteWidth, spritSheetModel.getmSpriteRowNb() * spriteHeight, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(result);
         Paint paint = new Paint();
 
         ArrayList<Sprite> spriteList = spritSheetModel.getSpriteList();
-        int spriteHeight = spritSheetModel.getSpriteHeight();
-        int spriteWidth = spritSheetModel.getSpriteWidth();
 
 
-        for(Sprite sprite:spriteList){
+        for (Sprite sprite : spriteList) {
             Bitmap[] spriteLayers = sprite.getLayers();
-            for(Bitmap bitmap:spriteLayers){
-                canvas.drawBitmap(bitmap, sprite.getmXposition()* spriteWidth,sprite.getmYposition()* spriteHeight,paint);
+            for (Bitmap bitmap : spriteLayers) {
+                canvas.drawBitmap(bitmap, sprite.getmXposition() * spriteWidth, sprite.getmYposition() * spriteHeight, paint);
             }
         }
 
 
         return result;
+    }
+
+    /**
+     * generate spritesheet(s) in an async task
+     *
+     * @param spriteSheetModelList
+     * @param callback             for progress update and result (in main thread)
+     */
+    public static void generateSpriteSheetAsync(final SpriteSheetGenerationListener callback, final SpriteSheet... spriteSheetModelList) {
+        new AsyncTask<String, Integer, Bitmap[]>() {
+            @Override
+            protected Bitmap[] doInBackground(String... params) {
+                Bitmap[] spritesheetResults = new Bitmap[spriteSheetModelList.length];
+                for (int i = 0; i < spriteSheetModelList.length; i++) {
+                    spritesheetResults[i] = generateSpriteSheet(spriteSheetModelList[i]);
+                    publishProgress((int) ((i / (float) (spriteSheetModelList.length)) * 100));
+                }
+                return spritesheetResults;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap... bitmap) {
+                callback.onSpriteSheetReady(bitmap);
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                callback.onProgressUpdate(values[0]);
+            }
+
+        }.execute();
+    }
+
+
+    public interface SpriteSheetGenerationListener {
+        void onSpriteSheetReady(Bitmap... bitmap);
+
+        void onProgressUpdate(int percentValue);
     }
 
 
